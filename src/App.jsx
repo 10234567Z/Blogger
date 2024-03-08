99975e2112126f48d160ab8bc9b10c1e66682af7
10234567Z <93607971+10234567Z@users.noreply.github.com>
2024-03-08 16:47:58 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>Upcoming : This website is to maintain your blog posts on bloggy</h1>
+    </>
+  )
+}
+
+export default App
